9bede46ffebeee559f14500461468caa4906c8ef
Mr. IO <43411416+Matthew-Hyndman@users.noreply.github.com>
2026-09-02 20:17:18 +0100

    Small changes for my time in Canada (#40)
    * small adjustments for my time in Canada :P

--- Backend/swift_booking/swift_booking/src/main/java/com/web/app/swift_booking/repository/CustomerRepository.java
+++ Backend/swift_booking/swift_booking/src/main/java/com/web/app/swift_booking/repository/CustomerRepository.java
@@ -1,12 +1,13 @@
 package com.web.app.swift_booking.repository;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.web.app.swift_booking.entity.Customer;
 
-import java.util.List;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 
     List<Customer> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCaseOrderByFirstNameAscLastNameAsc(
             String firstName,
